f7bf4f2d2d3ddff03c38236aca9d37937a5da255
furdog <138627600+furdog@users.noreply.github.com>
2025-12-29 16:12:03 +0200

    add more soh reset stuff

--- leaf_soh_reset_fsm.h
+++ leaf_soh_reset_fsm.h
@@ -1,7 +1,14 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <leaf_can_filter.h>
+
 /* ISO-TP leaf can filter SOH reset hardcoded emulation */
-#define LCF_SR_HEARDBEAT_RATE_MS 2000
+#define LCF_SR_HEARTBEAT_RATE_MS 2000u
 #define LCF_SR_TX_ID 0x79Bu
 #define LCF_SR_RX_ID 0x7BBu
+#define LCF_SR_RX_TIMEOUT_MS 2000u
 
 enum lcf_sr_state {
 	LCF_SR_STATE_STOPPED,
@@ -22,10 +29,7 @@ enum lcf_sr_state {
 
 	/* Default session */
 	LCF_SR_STATE_UDS_SESSION_DEFAULT,
-	LCF_SR_STATE_UDS_SESSION_DEFAULT_RESPONSE,
-
-	/* Await transmission */
-	LCF_SR_STATE_TX_AWAIT
+	LCF_SR_STATE_UDS_SESSION_DEFAULT_RESPONSE
 };
 
 struct lcf_sr {
@@ -37,16 +41,31 @@ struct lcf_sr {
 	bool _has_tx;
 	struct leaf_can_filter_frame _rx;
 	bool _has_rx;
+
+	uint32_t _timeout_ms;
+	uint32_t _timer_ms;
 };
 
 void lcf_sr_init(struct lcf_sr *self)
 {
-	self->state = 0u;
+	self->_state = 0u;
 
-	self->_heartbeat_clock_ms = LCF_SR_HEARDBEAT_RATE_MS;
+	self->_heartbeat_clock_ms =  LCF_SR_HEARTBEAT_RATE_MS;
 
 	self->_has_tx = false;
 	self->_has_rx = false;
+
+	self->_timeout_ms = 0u;
+	self->_timer_ms   = 0u;
+}
+
+void _lcf_sr_push_tx(struct lcf_sr *self, uint8_t *data, uint8_t len)
+{
+		/* Prepare TX frame for popping */
+		self->_has_tx = true;
+		self->_tx.id  = LCF_SR_TX_ID;
+		self->_tx.len = len;
+		(void)memcpy(self->_tx.data, data, 8u);
 }
 
 /** Push RX CAN frame for processing, returns false if busy. */
@@ -54,7 +73,7 @@ bool lcf_sr_push_frame(struct lcf_sr *self, struct leaf_can_filter_frame *f)
 {
 	bool result = false;
 
-	if (!self->_has_rx) {
+	if (!self->_has_rx && (f->id == LCF_SR_RX_ID)) {
 		self->_has_rx = true;
 
 		self->_rx = *f;
@@ -84,35 +103,198 @@ bool lcf_sr_pop_frame(struct lcf_sr *self, struct leaf_can_filter_frame *f)
 	return result;
 }
 
+void lcf_sr_start(struct lcf_sr *self)
+{
+	lcf_sr_init(self);
+
+	self->_state = LCF_SR_STATE_IDLE;
+}
+
+/** Silently fails */
+void lcf_sr_abort(struct lcf_sr *self)
+{
+	lcf_sr_init(self);
+}
+
+bool _lcf_sr_validate_response(struct lcf_sr *self, uint8_t *expec, size_t len)
+{
+	bool result = false;
+
+	/* Abort if no response */
+	if (self->_timeout_ms >= LCF_SR_RX_TIMEOUT_MS) {
+		lcf_sr_abort(self);
+
+	/* Ignore if no rx */
+	} else if (!self->_has_rx) {
+
+	/* Ignore if invalid message */
+	} else if ((self->_rx.len < len) || (self->_rx.data[0] != expec[0])) {
+
+	/* Abort if invalid response */
+	} else if (memcmp(self->_rx.data, expec, len) != 0) {
+		lcf_sr_abort(self);
+
+	/* Everything is fine in any other case */
+	} else {
+		result = true;
+	}
+
+	self->_has_rx = false;
+
+	return result;
+}
+
 void _lcf_sr_step(struct lcf_sr *self, uint32_t delta_time_ms)
 {
-	/* Switch to heartbeat if timeout and not doing anything */
-	if (self->_heartbeat_clock_ms >= LCF_SR_HEARDBEAT_RATE_MS) {
-		self->_heartbeat_clock_ms = 0u;
+	uint8_t state = self->_state;
 
-		if (self->_state == LCF_SR_STATE_IDLE) {
-			self->state = LCF_SR_STATE_UDS_TESTER_PRESENT;
+	self->_heartbeat_clock_ms += delta_time_ms;
+
+	/* Switch to heartbeat if timeout and not doing anything */
+	if (self->_heartbeat_clock_ms >= LCF_SR_HEARTBEAT_RATE_MS) {
+		self->_heartbeat_clock_ms -= LCF_SR_HEARTBEAT_RATE_MS;
+
+		if (self->_state == (uint8_t)LCF_SR_STATE_IDLE) {
+			state = LCF_SR_STATE_UDS_TESTER_PRESENT;
 		}
 	}
 
+	self->_timer_ms += delta_time_ms;
 
-	switch (self->_state) {
+	switch (state) {
+	case LCF_SR_STATE_IDLE:
+		if (self->_timer_ms >= (LCF_SR_HEARTBEAT_RATE_MS + 500u)) {
+			self->_state = LCF_SR_STATE_UDS_CALL_SERVICE0;
+		}
+
+		break;
+
 	case LCF_SR_STATE_UDS_TESTER_PRESENT: {
-		uint8_t data[8] = {0x02u, 0x3Eu, 0x01u, 0xFFu
+		uint8_t data[8] = {0x02u, 0x3Eu, 0x01u, 0xFFu,
 				   0xFFu, 0xFFu, 0xFFu, 0xFFu};
 		
-		self->_tx.id   = LCF_SR_TX_ID;
-		self->_tx.len  = 8u;
-		memcpy(self->_tx.data, data, 8u);
+		_lcf_sr_push_tx(self, data, 8u);
 
-		self->state = LCF_SR_STATE_TX_AWAIT;
-		self->_has_tx = true;
+		self->_timeout_ms = 0u;
+		self->_state = LCF_SR_STATE_UDS_TESTER_PRESENT_RESPONSE;
+		break;
+	}
+
+	/* Wait for positive response */
+	case LCF_SR_STATE_UDS_TESTER_PRESENT_RESPONSE: {
+		uint8_t expec[8] = {0x01u, 0x7Eu, 0xFFu, 0xFFu,
+				    0xFFu, 0xFFu, 0xFFu, 0xFFu};
+
+		self->_timeout_ms += delta_time_ms;
+
+		if (!_lcf_sr_validate_response(self, expec, sizeof(expec))) {
+			break;
+		}
+
+		self->_state = LCF_SR_STATE_IDLE;
+
+		break;
+	}
+
+	case LCF_SR_STATE_UDS_CALL_SERVICE0: {
+		uint8_t data[8] = {0x03u, 0x31u, 0x03u, 0x00u,
+				   0xFFu, 0xFFu, 0xFFu, 0xFFu};
+
+		_lcf_sr_push_tx(self, data, 8u);
+
+		self->_timeout_ms = 0u;
+		self->_state = LCF_SR_STATE_UDS_CALL_SERVICE0_RESPONSE;
+		break;
+	}
+
+	case LCF_SR_STATE_UDS_CALL_SERVICE0_RESPONSE: {
+		uint8_t expec[8] = {0x03u, 0x71u, 0x03u, 0x01u,
+				    0xFFu, 0xFFu, 0xFFu, 0xFFu};
+
+		self->_timeout_ms += delta_time_ms;
+
+		if (!_lcf_sr_validate_response(self, expec, sizeof(expec))) {
+			break;
+		}
+
+		self->_timer_ms = 0u;
+		self->_state = LCF_SR_STATE_UDS_CALL_SERVICE1;
+
+		break;
+	}
+
+	case LCF_SR_STATE_UDS_CALL_SERVICE1: {
+		uint8_t data[8] = {0x03u, 0x31u, 0x03u, 0x01u,
+				   0xFFu, 0xFFu, 0xFFu, 0xFFu};
+
+		/* Wait 50ms before service call */
+		if (self->_timer_ms < 50u) {
+			break;
+		}
+
+		_lcf_sr_push_tx(self, data, 8u);
+
+		self->_timeout_ms = 0u;
+		self->_state = LCF_SR_STATE_UDS_CALL_SERVICE1_RESPONSE;
+		break;
+	}
+
+	case LCF_SR_STATE_UDS_CALL_SERVICE1_RESPONSE: {
+		uint8_t expec[8] = {0x03u, 0x71u, 0x03u, 0x02u,
+				    0xFFu, 0xFFu, 0xFFu, 0xFFu};
+
+		self->_timeout_ms += delta_time_ms;
+
+		if (!_lcf_sr_validate_response(self, expec, sizeof(expec))) {
+			break;
+		}
+
+		self->_timer_ms = 0u;
+		self->_state = LCF_SR_STATE_UDS_SESSION_DEFAULT;
+
+		break;
+	}
+
+	case LCF_SR_STATE_UDS_SESSION_DEFAULT: {
+		uint8_t data[8] = {0x02u, 0x10u, 0x81u, 0xFFu,
+				   0xFFu, 0xFFu, 0xFFu, 0xFFu};
+
+		/* Wait 50ms before service call */
+		if (self->_timer_ms < 50u) {
+			break;
+		}
+
+		_lcf_sr_push_tx(self, data, 8u);
+
+		self->_timeout_ms = 0u;
+		self->_state = LCF_SR_STATE_UDS_SESSION_DEFAULT_RESPONSE;
+		break;
+	}
+
+	case LCF_SR_STATE_UDS_SESSION_DEFAULT_RESPONSE: {
+		uint8_t expec[8] = {0x02u, 0x50u, 0x81u, 0xFFu,
+				    0xFFu, 0xFFu, 0xFFu, 0xFFu};
+
+		self->_timeout_ms += delta_time_ms;
+
+		if (!_lcf_sr_validate_response(self, expec, sizeof(expec))) {
+			break;
+		}
+
+		self->_timer_ms = 0u;
+		lcf_sr_abort(self); /* Final step */
+
+		break;
+	}
+
+	default:
+		break;
 	}
 }
 
 void lcf_sr_step(struct lcf_sr *self, uint32_t delta_time_ms)
 {
-	if (self->_state != LCF_SR_STATE_STOPPED) {
+	if (self->_state != (uint8_t)LCF_SR_STATE_STOPPED) {
 		_lcf_sr_step(self, delta_time_ms);
 	}
 }
--- tests/make.sh
+++ tests/make.sh
@@ -4,13 +4,15 @@ if cd misra; then git pull; cd ..; else git clone "$MISRA_REPO"; fi
 MISRA='./MISRA/misra.sh'
 "$MISRA" setup
 
-TARGET="../leaf_can_filter.h"
+#TARGET="../leaf_can_filter.h"
+TARGET="../leaf_soh_reset_fsm.h"
 
 rm ./a* 2> /dev/null
 
 "$MISRA" check ${TARGET} "../libraries/bitE ../libraries/charge_counter ../libraries/iso_tp"
 
-gcc -I.. -I../libraries/bite/ -I../libraries/charge_counter/ "leaf_can_filter.test.c" -Wall -Wextra -g -std=c89 -pedantic \
+#gcc -I.. -I../libraries/bite/ -I../libraries/charge_counter/ "leaf_can_filter.test.c" -Wall -Wextra -g -std=c89 -pedantic \
+gcc -I.. -I../libraries/bite/ -I../libraries/charge_counter/ -I../libraries/iso_tp/ "soh_reset.test.c" -Wall -Wextra -g -std=c89 -pedantic \
 	-DBITE_DEBUG -DBITE_COLOR -DBITE_DEBUG_BUFFER_OVERFLOW -DBITE_PEDANTIC -DLEAF_CAN_FILTER_DEBUG
 #Enable pedantic mode, so assertions will trigger
 
